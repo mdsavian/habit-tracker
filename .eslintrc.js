3ee91f238dcfb27f4b9d975325c6c91e4c9f27b9
Marlon Savian <marlon.savian@mercadofavo.com>
2022-04-27 21:28:23 -0300

    chore: add jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,14 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,    
+    es2021: true,
+    'jest/globals': true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'prettier',
-  ],
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
   overrides: [
     {
       files: ['**/*.ts', '**/*.tsx'],
-    }
+    },
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -21,26 +18,32 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',    
-  ],
+  plugins: ['react', '@typescript-eslint', 'jest'],
   rules: {
     'import/extensions': 'off',
     'react/jsx-props-no-spreading': 'off',
     'import/no-extraneous-dependencies': 'off',
-    'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx', '.tsx', '.ts'] }],
-    "react/react-in-jsx-scope": "off",
+    'react/jsx-filename-extension': [
+      'warn',
+      { extensions: ['.js', '.jsx', '.tsx', '.ts'] },
+    ],
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
     'react/require-default-props': 'off',
     'react/no-danger': 'off',
-    'import/prefer-default-export': 'off',  
-    'no-unused-vars':'off'
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': 'off',
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx']
-      }
-    }
-  }
-};
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+}
