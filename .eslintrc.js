29f3c2a189848d3dc82cc4a8a96ead4db99bc233
Marlon Savian <marlon.savian@mercadofavo.com>
2022-05-05 13:02:36 -0300

    chore: add tsconfig modules

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,6 +45,9 @@ module.exports = {
       node: {
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
+      typescript: {
+        alwaysTryTypes: true, // always try to resolve types under `<roo/>@types` directory even it doesn't contain any source code, like `@types/unist`
+      },
     },
   },
 }
